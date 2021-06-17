2085f919b3e265640f297b4e0521614ed2e594db
nicoafd <dottinick@gmail.com>
2021-06-17 15:55:56 +0200

    iteration 6

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,24 +1,52 @@
 // Iteration #1: Find the maximum
-function maxOfTwoNumbers() {}
-
+function maxOfTwoNumbers(num1, num2) {
+  if (num1 > num2){
+    return num1;
+  }else { 
+    return num2;
+  }
+}
 
 
 // Iteration #2: Find longest word
 const words = ['mystery', 'brother', 'aviator', 'crocodile', 'pearl', 'orchard', 'crackpot'];
 
-function findLongestWord() {}
+function findLongestWord(words) {
+  if(!words.length){
+    return null;
+  }
+  let longestWord = "";
+  for (let i = 0; i < words.length; i++) {
+    if (longestWord.length < words[i].length){
+      longestWord = words[i];
+    }
+  }
+  return longestWord;
+}
 
 
 
 // Iteration #3: Calculate the sum
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
+function sumNumbers(numbers) {
+  let sum = 0
+  for (let i = 0; i < numbers.length; i++) {
+    sum = sum + numbers[i]
+  }
+  return sum;
+}
 
 
 
 // Iteration #3.1 Bonus:
-function sum() {}
+function sum(array) {
+  if ( array.length === 0){
+    return 0;
+
+  } 
+  return sum;
+}
 
 
 
@@ -26,7 +54,13 @@ function sum() {}
 // Level 1: Array of numbers
 const numbersAvg = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
+function averageNumbers(array) {
+  if (array.length === 0) {
+    return null;
+  }
+  const total = sumNumbers(array);
+  return total / array.length;
+}
 
 
 // Level 2: Array of strings
@@ -52,14 +86,33 @@ const wordsUnique = [
   'bring'
 ];
 
-function uniquifyArray() {}
+function uniquifyArray(wordsUnique) {
+
+  let newArray = []
+  for (let word of array){
+    if (!newArray.includes(word)) {
+      newArray.push(word)
+    }
+  }
+return newArray;
+}
 
 
 
 // Iteration #6: Find elements
 const wordsFind = ['machine', 'subset', 'trouble', 'starting', 'matter', 'eating', 'truth', 'disobedience'];
 
-function doesWordExist() {}
+function doesWordExist(arrayOfWords) {
+  let newArray = []
+  for (let word of arrayofWords){
+    if (if newArray.includes(word)){
+      return true;
+    } else {
+      return false;
+    }
+  }
+  return newArray;
+}
 
 
 
